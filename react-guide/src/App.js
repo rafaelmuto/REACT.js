65ff963e53ee3adf1dfae9d8782c8975f13b4a2d
Rafael Muto <r.nagahama@gmail.com>
2019-10-29 20:55:47 -0200

    function component props

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, this is a REACT app!</h1>
+        <hr />
+        <p>Occaecat proident exercitation enim dolore Lorem consectetur minim. Aliqua sit eiusmod adipisicing deserunt id fugiat non. Aute veniam magna do ullamco minim nostrud labore pariatur. Ut ad reprehenderit dolor nisi nostrud et.</p>
+        <hr />
+        <Person name="Rafael" age="33">
+          i like coding!
+        </Person>
+        <Person name="Juliana" age="25">
+          i like playing drums!
+        </Person>
       </div>
     );
   }
