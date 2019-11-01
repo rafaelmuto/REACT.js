fe5645667c299f5d2d77b6d99aeacc3049f5b7a9
Rafael Muto <r.nagahama@gmail.com>
2019-11-01 01:22:25 -0200

    new features

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -3,19 +3,41 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [{ name: 'Rafael', age: 33 }, { name: 'Juliana', age: 25 }, { name: 'Caio', age: 32 }]
+  };
+
+  deletePersonHandler = personIndex => {
+    const personsUpdate = this.state.persons;
+    personsUpdate.splice(personIndex, 1);
+    this.setState({ persons: personsUpdate });
+  };
+
+  togglePersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons });
+  };
+
   render() {
+    let personsList = null;
+
+    if (this.state.showPersons) {
+      personsList = this.state.persons.map((person, index) => {
+        return <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)} />;
+      });
+    }
+
     return (
       <div className="App">
+        <hr />
         <h1>Hi, this is a REACT app!</h1>
         <hr />
         <p>Occaecat proident exercitation enim dolore Lorem consectetur minim. Aliqua sit eiusmod adipisicing deserunt id fugiat non. Aute veniam magna do ullamco minim nostrud labore pariatur. Ut ad reprehenderit dolor nisi nostrud et.</p>
         <hr />
-        <Person name="Rafael" age="33">
-          i like coding!
-        </Person>
-        <Person name="Juliana" age="25">
-          i like playing drums!
-        </Person>
+
+        {personsList}
+
+        <button onClick={this.togglePersonsHandler}>click ME!</button>
+        <hr className="glyph" />
       </div>
     );
   }
--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Person.css';
 
 const person = props => {
   return (
-    <div>
-      <p>
-        I'm a {props.name}! I'm {props.age} years old.
-      </p>
-      <p>{props.children}</p>
+    <div className="Person">
+      <hr />
+      <h2>I'm a {props.name}.</h2>
+      <h4>I'm {props.age} years old.</h4>
+      <p>Lorem esse deserunt amet consectetur dolore voluptate commodo ut ea nulla dolor. Dolor excepteur qui ad ipsum sunt cupidatat amet exercitation laborum anim pariatur ad. Velit ullamco sit minim ex qui qui amet cupidatat. Cupidatat voluptate fugiat cupidatat ut reprehenderit cillum id labore cillum est sint officia.</p>
+
+      <input type="text" onChange={props.changed} value={props.name} />
+      <button onClick={props.click}>delete me!!!</button>
+
+      <hr className="glyph" />
     </div>
   );
 };
