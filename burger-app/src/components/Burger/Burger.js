81b259e8fe989e6d64ae3a9b30f09e4e9979c021
Rafael Muto <r.nagahama@gmail.com>
2019-11-10 21:14:51 -0200

    is the buger empty?

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import style from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
-    return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
-      return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p> please star adding ingredients! </p>;
+  }
 
   return (
     <div className={style.Burger}>
