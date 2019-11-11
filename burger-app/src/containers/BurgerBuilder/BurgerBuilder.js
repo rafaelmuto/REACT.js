87fe6a61ae7f23f2b771233b13aff9eea729c50c
Rafael Muto <r.nagahama@gmail.com>
2019-11-10 23:12:32 -0200

    burger builder controls

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+	salad: 0.5,
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7
+};
+
 class BurderBuilder extends Component {
 	constructor(props) {
 		super(props);
@@ -13,15 +20,51 @@ class BurderBuilder extends Component {
 				bacon: 0,
 				cheese: 0,
 				meat: 0
-			}
+			},
+			totalPrice: 4
 		};
 	}
 
+	addIngredientHandler = type => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCounted = oldCount + 1;
+		const updatedIngredients = { ...this.state.ingredients };
+		updatedIngredients[type] = updatedCounted;
+
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + INGREDIENT_PRICES[type];
+
+		this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
+	};
+
+	removeIngredientHandler = type => {
+		const oldCount = this.state.ingredients[type];
+		if (oldCount <= 0) {
+			return;
+		}
+		const updatedCounted = oldCount - 1;
+		const updatedIngredients = { ...this.state.ingredients };
+		updatedIngredients[type] = updatedCounted;
+
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - INGREDIENT_PRICES[type];
+
+		this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
+	};
+
 	render() {
+		const disabledInfo = { ...this.state.ingredients };
+		for (let key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0;
+		}
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
-				<BuildControls />
+				<BuildControls
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo}
+				/>
 			</Aux>
 		);
 	}
