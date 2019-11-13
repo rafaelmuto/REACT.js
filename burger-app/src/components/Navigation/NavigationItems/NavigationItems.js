40e70aa536ec60d1c1c1399ac68b9a1168bebae8
Rafael Muto <r.nagahama@gmail.com>
2019-11-13 01:33:09 -0200

    Navigation ItemS

---
+++ burger-app/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import style from './NavigationItems.module.css';
+
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = () => {
+	return (
+		<ul className={style.NavigationItems}>
+			<NavigationItem link="/" active>
+				Burger Builder
+			</NavigationItem>
+			<NavigationItem link="/">Checkout</NavigationItem>
+		</ul>
+	);
+};
+
+export default navigationItems;
--- burger-app/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,16 @@ import React from 'react';
 import style from './Toolbar.module.css';
 
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = props => {
 	return (
 		<header className={style.Toolbar}>
 			<div>MENU</div>
 			<Logo />
-			<nav>...</nav>
+			<nav>
+				<NavigationItems />
+			</nav>
 		</header>
 	);
 };
