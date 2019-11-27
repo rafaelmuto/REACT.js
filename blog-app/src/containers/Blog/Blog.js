ab71c579b95b465cea0e9713b0f7f93d6f1f927b
Rafael Muto <r.nagahama@gmail.com>
2019-11-27 02:12:53 -0200

    first react-router

--- blog-app/src/containers/Blog/Blog.js
+++ blog-app/src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 	render() {
@@ -19,7 +21,9 @@ class Blog extends Component {
 						</ul>
 					</nav>
 				</header>
-				<Posts />
+				{/* <Route path="/" exact render={() => <Posts />} /> */}
+				<Route path="/" exact component={Posts} />
+				<Route path="/new-post" component={NewPost} />
 			</div>
 		);
 	}
