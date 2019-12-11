295a15e2555029a1b8fa2d1fbc15cad546ec3d3c
Rafael Muto <r.nagahama@gmail.com>
2019-12-10 23:38:48 -0200

    checkout and checkoutSummary

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurderBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
-	return (
-		<div>
-			<Layout>
-				<BurderBuilder />
-			</Layout>
-		</div>
-	);
+  return (
+    <div>
+      <Layout>
+        <BurderBuilder />
+        <Checkout />
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
---
+++ burger-app/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import style from './CheckoutSummary.module.css';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+const checkoutSummary = props => {
+  return (
+    <div className={style.CheckoutSummary}>
+      <h1>We hope it tastes well!</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button btnType="Danger" clicked>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked>
+        CONTINUE
+      </Button>
+    </div>
+  );
+};
+
+export default checkoutSummary;
