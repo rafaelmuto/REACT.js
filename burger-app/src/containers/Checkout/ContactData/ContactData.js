3864997fe1b66d03a78aad65b942d77c1167052d
Rafael Muto <r.nagahama@gmail.com>
2019-12-13 00:55:25 -0200

    redirec after ordering

--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 import axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 import style from './ContactData.module.css';
 
@@ -39,6 +41,7 @@ class ContactData extends Component {
 			.post('/orders.json', order)
 			.then(res => {
 				this.setState({ loading: false });
+				this.props.history.push('/');
 			})
 			.catch(err => {
 				this.setState({ loading: false });
@@ -46,29 +49,27 @@ class ContactData extends Component {
 	};
 
 	render() {
+		let form = (
+			<form>
+				<input type="text" name="name" placeholder="your name here" />
+				<input type="text" name="email" placeholder="your email here" />
+				<input type="text" name="street" placeholder="your street name here" />
+				<input type="text" name="postal" placeholder="your postal code here" />
+				<Button btnType="Success" clicked={this.orderHandler}>
+					ORDER
+				</Button>
+			</form>
+		);
+		if (this.state.loading) {
+			form = <Spinner />;
+		}
 		return (
 			<div className={style.ContactData}>
 				<h4>Enter your Contact Data</h4>
-				<form>
-					<input type="text" name="name" placeholder="your name here" />
-					<input type="text" name="email" placeholder="your email here" />
-					<input
-						type="text"
-						name="street"
-						placeholder="your street name here"
-					/>
-					<input
-						type="text"
-						name="postal"
-						placeholder="your postal code here"
-					/>
-					<Button btnType="Success" clicked={this.orderHandler}>
-						ORDER
-					</Button>
-				</form>
+				{form}
 			</div>
 		);
 	}
 }
 
-export default ContactData;
+export default withRouter(ContactData);
